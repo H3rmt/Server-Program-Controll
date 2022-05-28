fa519176af0c1b76e26bb3a6bf29ef8fbc304524
H3rmt <stemmer.enrico@gmail.com>
2022-05-28 22:29:19 +0200

    added layout to login page

--- Website/index.php
+++ Website/index.php
@@ -12,33 +12,36 @@ $message = checkLogin()
 // db:  $2y$10$a9QQCHHEJ7bZ//D1My0oAOMRQXtRZIGV0YK5emhasFR9xusyBvyha
 ?>
 
-<form method="post">
-	<div><?= $message ?></div>
-	<div>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width">
+	<title>Login</title>
+	<link rel="stylesheet" href="login.css"/>
+	<link rel="stylesheet" href="mainstyle.css"/>
+</head>
+
+<body>
+
+<div id="login">
+	<form method="post">
 		<div>
 			<label for="username">Username</label>
+			<input id="username" name="username" type="text">
 		</div>
-		<div>
-			<label>
-				<input name="username" type="text" value="<?php if(isset($_COOKIE["username"])) {
-					echo $_COOKIE["username"];
-				} ?>">
-			</label>
-		</div>
-	</div>
-	<div>
 		<div>
 			<label for="password">Password</label>
+			<input id="password" name="password" type="password">
 		</div>
-		<div>
-			<label>
-				<input name="password" type="password">
-			</label>
-		</div>
-	</div>
-	<div>
-		<div>
+		<div id="bottom">
+			<h3 id="message"><?= $message ?></h3>
 			<input type="submit" name="login" value="Login">
 		</div>
-	</div>
-</form>
+	</form>
+</div>
+
+</body>
+
+</html>
--- Website/session.php
+++ Website/session.php
@@ -6,27 +6,27 @@ function checkSession(): bool {
 	// check if user already has a session
 	if(!empty($_COOKIE["username"]) && !empty($_COOKIE["hash"])) {
 		$session = getSession($_COOKIE["username"], $_COOKIE["hash"]);
-
+		
 		// no session found
 		if(!$session)
 			return false;
-
+		
 		list('ID' => $id, 'expire_date' => $expire_date) = $session;
-
-
+		
+		
 		if($expire_date >= date("Y-m-d H:i:s", time())) {
 			// authorise user, because session is valid
 			return true;
 		} else {
 			// drop expired session
 			dropSession($id);
-
+			
 			// drop invalid cookies for invalid session
 			setcookie("username", "");
 			setcookie("hash", "");
 			return false;
 		}
-
+		
 	}
 	return false;
 }
@@ -36,34 +36,37 @@ function checkLogin(): string {
 	if(!empty($_POST["login"])) {
 		$username = $_POST["username"];
 		$password = $_POST["password"];
-
+		
 		$member = getMember($username);
-
+		
 		// no member with username found
 		if(!$member)
-			return "Invalid Login {Username}";
-
-
+//			return "Invalid Login {Username}";
+			return "Invalid Login";
+		
+		
 		list('passwd' => $passwd) = $member;
 		if(password_verify($password, $passwd)) {
 			// login success
 			$current_time = time();
-
+			
 			$cookie_expiration_time = $current_time + getSetting('timeout') * 24 * 60 * 60;
-
+			
 			$hash = bin2hex(random_bytes(30));
 			setcookie("username", $username, $cookie_expiration_time);
 			setcookie("hash", $hash, $cookie_expiration_time);
-
+			
 			$expiry_date = date("Y-m-d H:i:s", $cookie_expiration_time);
-
+			
 			// Start new Session
 			createSession($username, $hash, $expiry_date);
 			header("refresh: 0"); //refresh to let checkSession detect session
 		} else {
-			return "Invalid Login {Password}"; // invalid password
+//			return "Invalid Login {Password}"; // invalid password
+			return "Invalid Login"; // invalid password
 		}
 	}
+	// no login / site just opened
 	return "";
 }
 
